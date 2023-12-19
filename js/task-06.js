7633eafe91199e7749e679a231b06b35277b5f9c
Artem Liskovshchenko <artem140742@gmail.com>
2023-12-19 17:16:34 +0200

    asd

--- js/task-06.js
+++ js/task-06.js
@@ -1,32 +1,55 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
 
 
 const createBtn = document.querySelector('.createBtn'),
-  fieldForBoxes = document.querySelector('#boxes'),
-  destroyBtn = document.querySelector('.destroyBtn'),
-  inputField = document.querySelector('.input');
+	fieldForBoxes = document.querySelector('#boxes'),
+	destroyBtn = document.querySelector('.destroyBtn'),
+	inputField = document.querySelector('.input'),
+	message = document.querySelector('.warning-message');
 
-function createBoxes(event) {
-  const boxesCount = inputField.value;
 
-  for (let i = 0; i < boxesCount; i++) {
-    const step = i * 10;
-    const gap = 20;
-    fieldForBoxes.innerHTML += `
-      <div style="
+function validateInput(value) {
+	let result = true;
+	if (value > 100 || value < 1) {
+		message.classList.add('visible');
+		!result;
+	} else {
+		message.classList.remove('visible');
+		result;
+	}
+	return result;
+}
+
+
+const createBoxes = () => {
+	destroyBoxes();
+	const boxesCount = inputField.value;
+	if (validateInput(boxesCount)) {
+		for (let i = 0; i < boxesCount; i++) {
+			const step = i * 10;
+			fieldForBoxes.innerHTML += `
+      <div class="box-item" style="
         width: ${30 + step}px;
         height: ${30 + step}px;
-        background-color: ${getRandomHexColor()};">
-      </div>
+		
+        background-color: ${getRandomHexColor()};"
+      ></div>
     `;
-  }
-  inputField.value = '';
+		}
+		inputField.value = '';
+	}
+}
+
+function destroyBoxes() {
+	while (fieldForBoxes.firstChild) {
+		fieldForBoxes.firstChild.remove();
+	}
 }
 
 
 createBtn.addEventListener('click', createBoxes);
-createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
